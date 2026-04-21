1b90571a0f52e53186399ea72f9bc9cf4a771883
A60874022 <a60874022@yandex.ru>
2026-04-21 20:09:49 +0500

    исправление ошибок

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
-from app.models.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
